a4a5cf7bc2ee5e46923763d04b3fc4deaa4e9823
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:04:51 +0100

    feat: initial commit for example-angular.ts

---
+++ example-angular.ts
@@ -0,0 +1,8 @@
+// Ví dụ với Angular
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Chào mừng đến với Angular!</h1>'
+})
+export class AppComponent {}
