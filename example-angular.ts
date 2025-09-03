0d9807a9b13a9f727e6a802188d31a2d8d23b0c9
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:05:47 +0100

    Update greeting message to be more inclusive.

--- example-angular.ts
+++ example-angular.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Chào mừng đến với Angular!</h1>'
+  template: '<h1>Chào mừng tất cả mọi người đến với Angular!</h1>'
 })
 export class AppComponent {}
