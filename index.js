1b5189d634ebcbcf878c714989ed2d900bd6ac79
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:05:27 +0100

    Update welcome message to be more inclusive.

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Entry point for the application
-console.log('Welcome to the Frontend Frameworks Vietnam community!');
+console.log('Welcome to the Frontend Frameworks Vietnam community! We are excited to have you!');
