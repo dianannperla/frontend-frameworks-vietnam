aa31166489b9bfbcf112172ce9b363cfb123be06
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:07:16 +0100

    Update welcome message to English for broader audience

--- example-vue.js
+++ example-vue.js
@@ -1,8 +1,8 @@
-// Ví dụ với Vue
+// Example with Vue
 import { createApp } from 'vue';
 
 const App = {
-  template: '<h1>Chào mừng bạn đến với Vue!</h1>'
+  template: '<h1>Welcome to Vue!</h1>'
 };
 
 createApp(App).mount('#app');
