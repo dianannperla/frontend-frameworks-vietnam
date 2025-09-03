3a63b940f32e4d0aa9a22c2e076c9150ba76193c
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:04:48 +0100

    feat: initial commit for example-react.js

---
+++ example-react.js
@@ -0,0 +1,9 @@
+// Ví dụ với React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Chào mừng đến với React!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
